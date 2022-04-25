fec4b7833038f5291c608712e6cc6eecd913eec2
ekaterina <demenkovakatarina@mail.ru>
2022-04-25 20:16:20 +0300

    feat: EduProgramStatisticsChartArc.vue, EduProgramStatisticsTable.vue, UserLink.vue, summary.vue

--- client/components/eleden/statistics/EduProgramStatisticsTable.vue
+++ client/components/eleden/statistics/EduProgramStatisticsTable.vue
@@ -6,81 +6,77 @@
           v-tooltip(bottom)
             template(#activator="{ on }")
               span(v-on="on") {{ (item[f].value / item[f].total * 100).toFixed(2) }}%
-            div Заполнено: {{ item[f].value }}
-            div Всего: {{ item[f].total }}
+            div {{ $t('statistics.eduProgramsStatistics.filled') }} {{ item[f].value }}
+            div {{ $t('statistics.eduProgramsStatistics.total') }} {{ item[f].total }}
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
+import type { PropType } from '#app'
 import { DataTableHeader } from 'vuetify/types'
 import { EduProgramStatisticsType } from '~/types/graphql'
+import { useI18n } from '~/composables'
 import TreeDataTable from '~/components/common/tables/TreeDataTable.vue'
 
-@Component<EduProgramStatisticsTable>({
+export default defineComponent({
   components: { TreeDataTable },
-  computed: {
-    headers (): DataTableHeader[] {
-      return [
-        { text: 'Направление подготовки', value: 'name' },
-        { text: 'Форма обучения', value: 'eduForm', align: 'center' },
-        { text: 'Год', value: 'admission', align: 'center' },
-        { text: 'Описание', value: 'description', align: 'center' },
-        { text: 'Учебный план', value: 'syllabus', align: 'center' },
-        { text: 'Календарный учебный график', value: 'calendar', align: 'center' },
-        { text: 'Авторы', value: 'users', align: 'center' },
-        { text: 'Аннотации', value: 'annotation', align: 'center' },
-        { text: 'Рабочие программы', value: 'work_program', align: 'center' },
-        { text: 'Методическое обеспечение', value: 'methodological_support', align: 'center' }
-      ]
-    },
-    formatColumns (): string[] {
-      return ['description', 'syllabus', 'calendar', 'users', 'annotation', 'work_program', 'methodological_support']
-    },
-    treeItems (): any {
-      return this.buildTreeItems(this.items)
+  props: {
+    items: { required: true, type: Array as PropType<EduProgramStatisticsType[]> }
+  },
+  setup (props) {
+    const { t } = useI18n()
+
+    const headers = computed<DataTableHeader[]>(() => ([
+      { text: t('statistics.eduProgramsStatistics.headers.name') as string, value: 'name' },
+      { text: t('statistics.eduProgramsStatistics.headers.eduForm') as string, value: 'eduForm', align: 'center' },
+      { text: t('statistics.eduProgramsStatistics.headers.admission') as string, value: 'admission', align: 'center' },
+      { text: t('statistics.eduProgramsStatistics.headers.description') as string, value: 'description', align: 'center' },
+      { text: t('statistics.eduProgramsStatistics.headers.syllabus') as string, value: 'syllabus', align: 'center' },
+      { text: t('statistics.eduProgramsStatistics.headers.calendar') as string, value: 'calendar', align: 'center' },
+      { text: t('statistics.eduProgramsStatistics.headers.users') as string, value: 'users', align: 'center' },
+      { text: t('statistics.eduProgramsStatistics.headers.annotation') as string, value: 'annotation', align: 'center' },
+      { text: t('statistics.eduProgramsStatistics.headers.work_program') as string, value: 'work_program', align: 'center' },
+      {
+        text: t('statistics.eduProgramsStatistics.headers.methodological_support') as string,
+        value: 'methodological_support',
+        align: 'center'
+      }
+    ]))
+
+    const formatColumns = computed<string[]>(() => (
+      ['description', 'syllabus', 'calendar', 'users', 'annotation', 'work_program', 'methodological_support']
+    ))
+
+    const treeItems = computed<any>(() => (buildTreeItems(props.items)))
+
+    const buildTreeItems = (items: EduProgramStatisticsType[], code: number = 0) => {
+      const currentCodes = [...new Set(items.map((e: EduProgramStatisticsType) => e.directionCode.split('.').slice(0, code + 1).join('.')))].sort()
+      return currentCodes.reduce((a: any, c: string) => {
+        const directionDown: boolean = code < c.split('.').length && code < 2
+        // Собираем верхушку
+        const n: any = {
+          id: c,
+          name: c,
+          children: directionDown
+            ? buildTreeItems(items.filter((e: EduProgramStatisticsType) => e.directionCode.substr(0, c.length) === c), code + 1)
+            : buildItemsValue(items.filter((e: EduProgramStatisticsType) => e.directionCode === c))
+        }
+        // Рассчитываем вверх
+        const calculations: any = ['description', 'syllabus', 'calendar', 'users', 'annotation', 'work_program', 'methodological_support'].reduce((a: any, c: string) => {
+          return { ...a, [c]: { value: n.children.reduce((a: number, current: any) => a + current[c].value, 0), total: n.children.reduce((a: number, current: any) => a + current[c].total, 0) } }
+        }, {})
+        return [...a, Object.assign(calculations, n)]
+      }, [])
     }
+
+    const buildItemsValue = (items: EduProgramStatisticsType[]) => {
+      return items.map((e: EduProgramStatisticsType) => {
+        return Object.assign(e.points.reduce((a: any, c: any) => {
+          return { ...a, [c.name]: { value: +c.value, total: +c.total } }
+        }, {}), e)
+      })
+    }
+
+    return { headers, formatColumns, treeItems, buildTreeItems, buildItemsValue }
   }
 })
-export default class EduProgramStatisticsTable extends Vue {
-  @Prop({ required: true, type: Array }) items!: EduProgramStatisticsType[]
-  formatColumns!: string[]
-
-  buildTreeItems (items: EduProgramStatisticsType[], code: number = 0) {
-    const currentCodes = [...new Set(items.map((e: EduProgramStatisticsType) => e.directionCode.split('.').slice(0, code + 1).join('.')))].sort()
-    return currentCodes.reduce((a: any, c: string) => {
-      const directionDown: boolean = code < c.split('.').length && code < 2
-      // Собираем верхушку
-      const n: any = {
-        id: c,
-        name: c,
-        children: directionDown
-          ? this.buildTreeItems(items.filter((e: EduProgramStatisticsType) => e.directionCode.substr(0, c.length) === c), code + 1)
-          : this.buildItemsValue(items.filter((e: EduProgramStatisticsType) => e.directionCode === c))
-      }
-      // Рассчитываем вверх
-      const calculations: any = ['description', 'syllabus', 'calendar', 'users', 'annotation', 'work_program', 'methodological_support'].reduce((a: any, c: string) => {
-        return { ...a, [c]: { value: n.children.reduce((a: number, current: any) => a + current[c].value, 0), total: n.children.reduce((a: number, current: any) => a + current[c].total, 0) } }
-      }, {})
-      return [...a, Object.assign(calculations, n)]
-    }, [])
-  }
-
-  buildItemsValue (items: EduProgramStatisticsType[]) {
-    return items.map((e: EduProgramStatisticsType) => {
-      return Object.assign(e.points.reduce((a: any, c: any) => {
-        return { ...a, [c.name]: { value: +c.value, total: +c.total } }
-      }, {}), e)
-    })
-  }
-
-  /**
-   * Получение перевода относильно локального пути
-   * @param path
-   * @param values
-   * @return
-   */
-  t (path: string, values: any = undefined): string {
-    return this.$t(`statistics.eduProgramsStatistics.${path}`, values) as string
-  }
-}
 </script>

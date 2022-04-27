9a83184bac5b66842dbb726c16c29891d442f85c
ekaterina <demenkovakatarina@mail.ru>
2022-04-27 17:01:27 +0300

    fix: правки

--- init.py
+++ init.py
@@ -1,2 +1,14 @@
-#!/bin/bash
-ln -s ~/PycharmProjects/eleden/server/storage  ~/PycharmProjects/eleden/client/static/storage
+import os
+from os import path
+from pathlib import Path
+
+
+def init() -> None:
+    root = Path(__file__).resolve(strict=True).parent
+    server_storage = path.join(root, 'server', 'storage')
+    client_storage = path.join(root, 'client', 'static', 'storage')
+    os.symlink(server_storage, client_storage)
+
+
+if __name__ == '__main__':
+    init()

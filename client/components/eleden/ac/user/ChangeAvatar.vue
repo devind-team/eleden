94e73960eafaff566b03fec0e8e1cd2f4449f5ff
Luferov Victor <lyferov@yandex.ru>
2022-05-12 13:01:42 +0300

    fix: Исправлено изменение аватара

--- client/components/eleden/ac/user/ChangeAvatar.vue
+++ client/components/eleden/ac/user/ChangeAvatar.vue
@@ -1,90 +1,60 @@
 <template lang="pug">
   .d-flex.flex-column.align-center
     avatar-dialog(:item="user" size="200")
-    v-dialog(v-model="active" width="600")
+    mutation-modal-form(
+      :mutation="require('~/gql/core/mutations/user/change_avatar.graphql')"
+      :variables="{ userId: user.id, file }"
+      :update="update"
+      :header="String($t('ac.users.personalities.changeAvatar.chooseAvatar'))"
+      :button-text="String($t('ac.users.personalities.changeAvatar.buttons.load'))"
+      mutation-name="changeAvatar"
+      i18n-path="ac.users.personalities.changeAvatar"
+    )
       template(#activator="{ on }")
         v-btn(
           v-if="user.change"
           v-on="on"
           color="success"
         ).mt-3 {{ $t('ac.users.personalities.changeAvatar.buttons.changeAvatar') }}
-      apollo-mutation(
-        :mutation="require('~/gql/core/mutations/user/change_avatar.graphql')"
-        :variables="{ userId: user.id, file }"
-        :update="update"
-        @done="changeAvatarDone")
-        template(v-slot="{ mutate, error, loading }")
-          ValidationObserver(v-slot="{ handleSubmit, invalid }" ref="changeAvatar")
-            form(@submit.prevent="handleSubmit(mutate)")
-              v-card
-                v-card-title {{ $t('ac.users.personalities.changeAvatar.chooseAvatar') }}
-                  v-spacer
-                  v-btn(@click="active = false" icon)
-                    v-icon mdi-close
-                v-card-text
-                  ValidationProvider(
-                    :name="$t('ac.users.personalities.changeAvatar.avatar')"
-                    rules="required"
-                    v-slot="{ errors, valid }"
-                    tag="div"
-                    )
-                    v-file-input(
-                      v-model="file"
-                      :label="$t('ac.users.personalities.changeAvatar.avatar')"
-                      :error-messages="errors"
-                      :success="valid"
-                      prepend-icon="mdi-camera"
-                      show-size
-                      )
-                v-card-actions
-                  v-spacer
-                  v-btn(
-                    type="submit"
-                    :loading="loading"
-                    :disabled="invalid"
-                    color="primary"
-                  ) {{ $t('ac.users.personalities.changeAvatar.buttons.load') }}
+      template(#form)
+        ValidationProvider(
+          :name="String($t('ac.users.personalities.changeAvatar.avatar'))"
+          rules="required"
+          v-slot="{ errors, valid }"
+          tag="div"
+          )
+          v-file-input(
+            v-model="file"
+            :label="$t('ac.users.personalities.changeAvatar.avatar')"
+            :error-messages="errors"
+            :success="valid"
+            prepend-icon="mdi-camera"
+            show-size
+          )
 </template>
 
 <script lang="ts">
 import type { PropType } from '#app'
+import { defineComponent, ref } from '#app'
 import { DataProxy } from 'apollo-cache'
 import { ValidationObserver } from 'vee-validate'
-import { camelCase } from 'scule'
-import { ChangeAvatarMutationPayload, ErrorFieldType, UserType } from '~/types/graphql'
-import { useI18n } from '~/composables'
+import { UserType } from '~/types/graphql'
 import AvatarDialog from '~/components/users/AvatarDialog.vue'
+import MutationModalForm from '~/components/common/forms/MutationModalForm.vue'
 
 type UpdateAvatarType = (store: DataProxy, result: any) => void
 type ChangeAvatarType = InstanceType<typeof ValidationObserver> | null
 
 export default defineComponent({
-  components: { AvatarDialog },
+  components: { MutationModalForm, AvatarDialog },
   props: {
     update: { required: true, type: Function as PropType<UpdateAvatarType> },
     user: { required: true, type: Object as PropType<UserType> }
   },
   setup () {
-    const { t } = useI18n()
-
     const changeAvatar = ref<ChangeAvatarType>(null)
     const file = ref<File | null>(null)
-    const active = ref<boolean>(false)
-
-    const changeAvatarDone =
-      ({ data: { changeAvatar: { success, errors } } }: { data: { changeAvatar: ChangeAvatarMutationPayload } }) => {
-        file.value = null
-        if (success) {
-          active.value = false
-        } else {
-          changeAvatar.value.setErrors(errors.reduce(
-            (a: { [key: string]: string[] }, c: ErrorFieldType) => {
-              return { ...a, [t(`profile.${camelCase(c.field)}`) as string]: c.messages }
-            }, {}))
-        }
-      }
-
-    return { changeAvatar, file, active, changeAvatarDone }
+    return { changeAvatar, file }
   }
 })
 </script>
